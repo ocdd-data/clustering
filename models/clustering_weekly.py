54f9e9a1b936954fcf4b5239ae092cb748c2dfe4
Huang Ziyan <120101655+mochasteammilk@users.noreply.github.com>
2026-04-30 11:06:49 +0800

    Update clustering_weekly with normalisation

--- models/clustering_weekly.py
+++ models/clustering_weekly.py
@@ -8,11 +8,11 @@ from utils.helpers import Query
 # Load model definitions
 MODELS_DIR = Path(__file__).resolve().parent
 
-scaler_def = pd.read_csv(MODELS_DIR / "scaler_definitions.csv")
+scaler_def = pd.read_csv(MODELS_DIR / "SG" / "scaler_params.csv")
 scaler_mean = scaler_def.set_index("Feature")["Mean"]
 scaler_var = scaler_def.set_index("Feature")["Variance"]
 
-cluster_def = pd.read_csv(MODELS_DIR / "cluster_definitions.csv")
+cluster_def = pd.read_csv(MODELS_DIR / "SG" / "cluster_centroids.csv")
 cluster_def['Centroid'] = cluster_def['Centroid'].apply(lambda x: [float(i) for i in re.findall(r"[-+]?\d*\.\d+|\d+", x)])
 cluster_centroids = np.array(cluster_def['Centroid'].tolist())
 cluster_labels = dict(zip(cluster_def["Cluster"], cluster_def["Description"]))
@@ -25,7 +25,7 @@ def scale_features(df):
 
 
 def assign_clusters(df):
-    X = df[['frequency_scaled', 'total_scaled', 'rate_scaled']].values
+    X = df[['count_rate_scaled', 'trip_rate_scaled', 'avg_scaled']].values
     distances = np.linalg.norm(X[:, None, :] - cluster_centroids[None, :, :], axis=2)
     df['Cluster'] = distances.argmin(axis=1)
     return df
@@ -49,13 +49,17 @@ def get_redash_data(start, end, redash, query_id, return_cluster_stats=False):
         return (pd.DataFrame(), pd.DataFrame()) if return_cluster_stats else pd.DataFrame()
 
     agg_df = df.groupby('rider_uuid').agg(
-        frequency=('frequency', 'sum'),
-        total=('total', 'sum')
+        count_rate=('frequency', 'sum'),
+        trip_rate=('total', 'sum')
     ).reset_index()
-    
-    agg_df = agg_df[agg_df['frequency'] > 0]
-    agg_df['rate'] = agg_df['total'] / agg_df['frequency']
-    agg_df = agg_df.dropna(subset=['rate'])
+
+    agg_df = agg_df[agg_df['count_rate'] > 0]
+    agg_df['avg'] = agg_df['trip_rate'] / agg_df['count_rate']
+    agg_df = agg_df.dropna(subset=['avg'])
+
+    days_in_period = (end - start).days + 1
+    agg_df['count_rate'] = agg_df['count_rate'] / days_in_period
+    agg_df['trip_rate'] = agg_df['trip_rate'] / days_in_period
 
     agg_df = scale_features(agg_df)
     agg_df = assign_clusters(agg_df)
@@ -65,7 +69,7 @@ def get_redash_data(start, end, redash, query_id, return_cluster_stats=False):
 
     if return_cluster_stats:
         cluster_stats = agg_df.groupby('Cluster_Description').agg({
-            'total': 'sum'
+            'trip_rate': 'sum'
         }).round(2).T
         cluster_stats.index = [(end + timedelta(days=1)).strftime("%Y-%m-%d")]
         return agg_df, cluster_stats
